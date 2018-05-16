ef7fff79773d6398cc42b8acde254096c1eaa391
mooromets <sergey.sambor@gmail.com>
2018-05-16 18:20:52 +0200

    iMarco script generation

--- sharethedrama/sharethedrama.R
+++ sharethedrama/sharethedrama.R
@@ -1,4 +1,7 @@
 library(dplyr)
+library(readtext)
+library(plyr)
+
 dat <- read.csv("names.csv")
 
 # extract first and last names from the name string
@@ -18,7 +21,7 @@ code <- temp[, "code"]
 city <- temp[, "city"]
 
 #generate users
-num <- 20
+num <- 4
 ldply  (sample(c("m", "f"), num, replace = TRUE),
         .fun = function(sex){
           fn <- sample(with(dat, Firstname[name1 == sex]), 1)
@@ -32,7 +35,34 @@ ldply  (sample(c("m", "f"), num, replace = TRUE),
                                  1),
                           "@",
                           sample(c("ukr.net", "i.ua", "email.ua", "mail.ua", "yandex.ua"), 1))
-          phone <- sample(code, 1) *10000000 + sample(1:10000000, 1)
+          phone <- sprintf("+38(0%d)%03d-%03d-%02d", 
+                           sample(code, 1),
+                           sample(1:1000, 1),
+                           sample(1:100, 1),
+                           sample(1:100, 1))
           city <- sample(as.character(city), 1)
           return(c(paste(fn, ln), email, city, phone))
-        })
+        }) -> users
+
+#get the macros
+macros <- scan(file = "sample.iim", what = character(), sep = "\n")
+
+#process all users
+apply(users,
+      1,
+       FUN = function(u){
+         sapply(macros,
+                USE.NAMES = FALSE,
+                FUN = function(s) {
+                  s <- gsub("sample@mail.net", u[2], s)
+                  s <- gsub("john<SP>doe", gsub(" ", "<SP>", u[1]), s)
+                  s <- gsub("kyiv", u[3], s)
+                  s <- gsub("38phone", u[4], s)
+                })
+         }) -> script
+
+write(as.vector(script), "difference.iim") 
+
+#write log
+oldUsers <- read.csv("log.txt", header = FALSE)
+write.csv(rbind(oldUsers, users), "log.txt", row.names = FALSE)
