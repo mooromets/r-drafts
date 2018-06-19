6d35cb72a4ff8a45b43db0a52174bf00086b6729
mooromets <sergey.sambor@gmail.com>
2018-06-19 14:15:38 +0200

    helper functions added

--- it-salary/plot.R
+++ it-salary/plot.R
@@ -11,6 +11,12 @@ mysel <- data.frame(
 
 plot(x = mysel$date, y = mysel$value, type = "l")
 
+expToDate <- function(date) {
+  theDate <- as.Date(date)
+  last_recored <- max(mysel0$date[mysel0$date < theDate])
+  mysel0[mysel0$date == last_recored, "totexp"] + as.integer(theDate - last_recored) / 365
+}
+
 #impute additional info
 mysel2 <- data.frame(
   totexp = mysel$totexp,
@@ -26,52 +32,28 @@ mysel0 <- merge(mysel,
 
 plot(x = mysel0$date, y = mysel0$value, type = "l")
 
-
 Sys.setlocale("LC_ALL", "Russian_Russia")
 
+
 #october2010
-
-#my exp to the date
-theDate <- as.Date("2010-10-15")
-last_recored <- max(mysel0$date[mysel0$date < theDate])
-myExp <- mysel0[mysel0$date == last_recored, "totexp"] + as.integer(theDate - last_recored) / 365
-
-expPrecision <- 0.2
-  
 sal <- read.csv("./data/dou/2010_october_clean.csv", encoding = "UTF-8")
-sal <- sal[,c(2, 4, 5)] 
-names(sal) <- c("exp", "lang", "sal")
+names(sal)[c(1, 2, 4, 5)] <- c("loc", "exp", "lang", "sal")
 sal %>%
-  select()
-  filter(lang == "C++" | lang == "C") %>%
-  filter(between(exp, 
-                 round(myExp * (1 - expPrecision), 1), 
-                 round(myExp * (1 + expPrecision), 1))) %>%
+  filterLang() %>%
+  filterExp(expVal = expToDate("2010-10-15")) %>%
+  filterCity(locs = "другой") %>%
   select(sal) -> dat
+mean(dat$sal, na.rm = TRUE)
+median(dat$sal, na.rm = TRUE)
 
-mean(dat$sal)
-median(dat$sal)
 
 #may2011
-
-#my exp to the date
-theDate <- as.Date("2011-05-15")
-last_recored <- max(mysel0$date[mysel0$date < theDate])
-myExp <- mysel0[mysel0$date == last_recored, "totexp"] + as.integer(theDate - last_recored) / 365
-
-expPrecision <- 0.2
-
 sal <- read.csv("./data/dou/2011_may_final.csv", encoding = "UTF-8")
-sal <- sal[,c(6, 10, 12, 17)]
-names(sal) <- c("lang", "exp", "sal", "loc")
-sal$exp <- as.numeric(as.character(sal$exp))
+names(sal)[6] <- "lang"
 sal %>%
-  filter((lang == "C++" | lang == "C")) %>%
-  filter(between(exp, 
-                 round(myExp * (1 - expPrecision), 1), 
-                 round(myExp * (1 + expPrecision), 1))) %>%
-  filter(loc == "Киев")%>%
-  select(sal) -> dat
-
+  filterLang() %>%
+  filterExp(expVal = expToDate("2011-05-15")) %>%
+  filterCity(locs = "other") %>%
+  select(salary) -> dat
 mean(dat$sal)
 median(dat$sal)
