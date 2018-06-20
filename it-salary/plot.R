d1b4ca19082f330e45a7b8ba845a105853ec42d3
mooromets <sergey.sambor@gmail.com>
2018-06-20 22:11:13 +0200

    general calculateSal function

--- it-salary/plot.R
+++ it-salary/plot.R
@@ -1,6 +1,30 @@
 library(dplyr)
+library(lubridate)
+Sys.setlocale("LC_ALL", "Russian_Russia")
 
-#origin
+#
+#filters
+#
+filterExp <- function(.data, expVal, prec = .2) {
+  filter(.data, between(exp, 
+                        round(expVal * (1 - prec), 1), 
+                        round(expVal * (1 + prec), 1)))
+}
+
+filterLang <- function(.data, langs = c("C++", "C")) {
+  filter(.data, lang %in% langs)
+}
+
+filterCity <- function(.data, locs = "Киев") {
+  filter(.data, loc %in% locs)
+}
+
+filterRoleclass <- function(.data, class = "DEV") {
+  filter(.data, cls == class)
+}
+
+
+#origin data
 
 mysel <- data.frame(
   value = c(250, 300, 1100, 1700, 2000, 2800, 3200, 3200),
@@ -9,13 +33,8 @@ mysel <- data.frame(
                    "2013-05-15", "2014-02-15", "2015-09-15", "2016-09-15"))
 )
 
-plot(x = mysel$date, y = mysel$value, type = "l")
+#plot(x = mysel$date, y = mysel$value, type = "l")
 
-expToDate <- function(date) {
-  theDate <- as.Date(date)
-  last_recored <- max(mysel0$date[mysel0$date < theDate])
-  mysel0[mysel0$date == last_recored, "totexp"] + as.integer(theDate - last_recored) / 365
-}
 
 #impute additional info
 mysel2 <- data.frame(
@@ -30,13 +49,56 @@ mysel0 <- merge(mysel,
                sort = TRUE, 
                all = TRUE)
 
-plot(x = mysel0$date, y = mysel0$value, type = "l")
+#plot(x = mysel0$date, y = mysel0$value, type = "l")
 
-Sys.setlocale("LC_ALL", "Russian_Russia")
+#calculate experience to date
+expToDate <- function(date) {
+  theDate <- as.Date(date)
+  last_recored <- max(mysel0$date[mysel0$date < theDate])
+  mysel0[mysel0$date == last_recored, "totexp"] + as.integer(theDate - last_recored) / 365
+}
+
+
+calculateSal <- function(datafile) {
+  #get date from filename
+  uspos <- gregexpr("_", datafile)
+  
+  thisDate <- ymd(paste0(
+    as.integer(substr(datafile, 1, 4)),
+    substr(datafile, uspos[[1]][1] + 1, uspos[[1]][2] - 1),
+    "15"
+  ))
+  
+  #read data from file
+  sal <- read.csv(paste0("./data/dou/", datafile), encoding = "UTF-8")
+  
+  #most common case
+  if (thisDate >= as.Date("2011-09-15")) {
+    names(sal)[which(names(sal) == "Язык.программирования")] <- "lang"
+    sal %>%
+      filterLang() %>%
+      filterExp(expVal = expToDate(thisDate)) %>%
+      filterCity() %>%
+      select(salary) -> dat
+    qua <- quantile(dat$sal, na.rm = TRUE)
+    data.frame(date = thisDate, p25 = qua[2], p50 = qua[3], p75 = qua[4])
+  }
+}
+
+
+#files list
+fl <- dir("data/dou/")[10+3*(0:11)]
+
+#get results
+do.call("rbind", lapply(fl[1:10], FUN = calculateSal))
+
+
 
 #salary quantiles
 sq <- data.frame()
 dates <- c()
+
+
 
 #october2010
 sal <- read.csv("./data/dou/2010_october_clean.csv", encoding = "UTF-8")
@@ -94,7 +156,7 @@ dates <- c(dates, "2010-10-15")
 
 #may2011
 sal <- read.csv("./data/dou/2011_may_final.csv", encoding = "UTF-8")
-names(sal)[6] <- "lang"
+names(sal)[which(names(sal) == "Язык.программирования")] <- "lang"
 sal %>%
   filterRoleclass() %>%
   filterExp(expVal = expToDate("2011-05-15")) %>%
@@ -106,136 +168,17 @@ dates <- c(dates, "2011-05-15")
 
 #dec2011
 sal <- read.csv("./data/dou/2011_dec_final.csv", encoding = "UTF-8")
-names(sal)[7] <- "lang"
+names(sal)[which(names(sal) == "Язык.программирования")] <- "lang"
 sal %>%
   filterLang() %>%
   filterExp(expVal = expToDate("2011-12-15")) %>%
-  filterCity() %>%
+  filterCity(locs = "Киев") %>%
   select(salary) -> dat
 print(dim(dat))
 sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
 dates <- c(dates, "2011-12-15")
 
-#may2012
-sal <- read.csv("./data/dou/2012_may_final.csv", encoding = "UTF-8")
-names(sal)[7] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2012-05-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2012-05-15")
-
-#dec2012
-sal <- read.csv("./data/dou/2012_dec_final.csv", encoding = "UTF-8")
-names(sal)[3] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2012-12-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2012-12-15")
-
-#may2013
-sal <- read.csv("./data/dou/2013_may_final.csv", encoding = "UTF-8")
-names(sal)[3] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2013-05-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2013-05-15")
-
-#dec2013
-sal <- read.csv("./data/dou/2013_dec_final.csv", encoding = "UTF-8")
-names(sal)[3] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2013-12-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2013-12-15")
-
-#may2014
-sal <- read.csv("./data/dou/2014_may_final.csv", encoding = "UTF-8")
-names(sal)[2] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2014-05-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2014-05-15")
-
-#dec2014
-sal <- read.csv("./data/dou/2014_dec_final.csv", encoding = "UTF-8")
-names(sal)[2] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2014-12-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2014-12-15")
-
-#may2015
-sal <- read.csv("./data/dou/2015_may_final.csv", encoding = "UTF-8")
-names(sal)[2] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2015-05-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2015-05-15")
-
-#dec2015
-sal <- read.csv("./data/dou/2015_dec_final.csv", encoding = "UTF-8")
-names(sal)[3] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2015-12-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2015-12-15")
-
-#may2016
-sal <- read.csv("./data/dou/2016_may_final.csv", encoding = "UTF-8")
-names(sal)[3] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2016-05-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2016-05-15")
-
-#dec2016
-sal <- read.csv("./data/dou/2016_dec_final.csv", encoding = "UTF-8")
-names(sal)[3] <- "lang"
-sal %>%
-  filterLang() %>%
-  filterExp(expVal = expToDate("2016-12-15")) %>%
-  filterCity() %>%
-  select(salary) -> dat
-print(dim(dat))
-sq <- rbind(sq, print(quantile(dat$sal, na.rm = TRUE)))
-dates <- c(dates, "2016-12-15")
-
+# ... #
 
 sq["dates"] <- as.Date(dates)
 names(sq)[1:5] <- c("min", "p25", "med", "p75", "max")
