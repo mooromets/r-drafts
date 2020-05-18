aacace3e6a70b7ccfb07503b18c15af44157693c
mooromets <sergey.sambor@gmail.com>
2020-05-18 08:11:05 +0200

    update data preparation

--- duplicate_files/prepare_own_data.R
+++ duplicate_files/prepare_own_data.R
@@ -23,3 +23,27 @@ df <- dplyr::select(df, -c(atime, isdir, mode, exe, X))
 # save clean data
 write.csv(df, DEF_DATA, fileEncoding = ENCODING)
 
+# create frequecy table
+df_freq <-
+  group_by(df, md5, size) %>%
+  summarise(
+    n = n()
+  ) %>%
+  arrange(desc(n))
+
+#amount of space wasted (MB)
+(sum(df_freq$size*(df_freq$n-1)))
+
+# update freq column
+df <- left_join(df,
+                df_freq[,c("md5","n")],
+                by = c("md5"))
+
+freq_dirs <- filter(df, n > 1) %>%
+  group_by(dirname) %>%
+  summarize(
+    nFiles = n(),
+    sumSize = sum(size)
+  ) %>%
+  arrange(desc(sumSize))
+
